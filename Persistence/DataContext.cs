bfdad695badd94af0eec766a3ed0d7887526eef1
Jonas Retzius <retzius@vigilo.no>
2023-08-23 11:14:06 +0200

    End of section 2

---
+++ Persistence/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+using Activity = Domain.Activities.Activity;
+
+namespace Persistence
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+       public DbSet<Activity> Activities { get; set; }
+    }
+}
